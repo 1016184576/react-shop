d680dc4308f36051b1a8e23daa637ee193bebfb3
1016184576 <miaozheng.0406@qq.com>
2020-05-13 16:12:37 +0800

    添加头部header公共组件

---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.scss'
+
+export default class indexx extends Component {
+  render() {
+    return (
+      <div className="header">
+        
+      </div>
+    )
+  }
+}
--- src/pages/home/cart/index.jsx
+++ src/pages/home/cart/index.jsx
@@ -1,21 +1,60 @@
 import React from 'react';
+import { Chart } from '@antv/g2';
 import './index.scss';
 
 
 class CartComponent extends React.Component {
 
   componentDidMount() {
-    this.initChart()
+    this.renderChart()
   }
 
-  initChart() {
+
+
+  renderChart() {
+    const data = [
+      { year: '1951 年', sales: 38 },
+      { year: '1952 年', sales: 52 },
+      { year: '1956 年', sales: 61 },
+      { year: '1957 年', sales: 145 },
+      { year: '1958 年', sales: 48 },
+      { year: '1959 年', sales: 38 },
+      { year: '1960 年', sales: 38 },
+      { year: '1962 年', sales: 38 },
+    ];
+
+    let chart = new Chart({
+      container: 'c1',
+      autoFit: false,
+      width: 800,
+      height: 600,
+    });
+
+    chart.data(data);
+
+    chart.scale('year', {
+      formatter: val => val.replace(' 年', '')
+    });
+
+    chart.scale('sales', {
+      ticks: [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 20],
+      formatter: val => `￥${val}`
+    });
+
+    chart.axis('sales', {
+      title: {},
+    });
+
+    chart.interval().position('year*sales');
+
+    chart.render();
   }
 
   render() {
     return (
       <div>
         <p>购物车</p>
-        <div id="mountNode" style={{marginLeft: '100px'}}>
+        <div id="mountNode" style={{ marginLeft: '100px' }}>
         </div>
       </div>
     )
